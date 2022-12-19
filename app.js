f6972be03dad07752c6311f7de275bf35fc8ca41
SCWFoxy <121032351+SCWFoxy@users.noreply.github.com>
2022-12-19 15:38:22 +0000

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/demo/:id', (req, res) => {
-    eval('console.log("something", ' + req.params.id + ')')
-    res.send(`Hello ${req.params.id}`)
+    res.send(`Hello Guest`)
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
