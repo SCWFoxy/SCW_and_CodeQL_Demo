9e463cf6501231c8df5356abc3f8bcf65161e46a
SCWFoxy <121032351+SCWFoxy@users.noreply.github.com>
2023-01-16 13:02:32 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-//Comment Line
+//Comment Line Modded to kick off a scan
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/demo/:id', (req, res) => {
     res.send('Hello ${Req.params.id}')
