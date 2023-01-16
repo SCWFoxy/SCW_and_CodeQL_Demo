a89721a89abba47c0f19ae725932deb0e0eab8e6
SCWFoxy <121032351+SCWFoxy@users.noreply.github.com>
2023-01-16 13:08:41 +0000

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express()
 const port = 3000
 
 //Comment Line
-// Emilys special comment
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/demo/:id', (req, res) => {
     res.send(`Hello Guest`)
