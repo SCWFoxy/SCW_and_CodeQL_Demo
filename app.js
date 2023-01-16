0c3aa9a6725232024a351f4cf822d5ef254c869e
SCWFoxy <121032351+SCWFoxy@users.noreply.github.com>
2023-01-16 12:54:48 +0000

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ const app = express()
 const port = 3000
 
 //Comment Line
-// Emilys special comment
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/demo/:id', (req, res) => {
-    res.send(${Req.params.id})
+    res.send('Hello ${Req.params.id}')
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
