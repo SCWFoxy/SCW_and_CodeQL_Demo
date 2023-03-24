ca6b4a25d4c24d4f383879968a7fd4e944dcacdc
SCWFoxy <121032351+SCWFoxy@users.noreply.github.com>
2023-03-24 12:38:57 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-//Comment Line
+//Modify the code below by uncommenting the line with the unsafe use of a parameter and commenting the line that uses the text Hello Guest
+
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/demo/:id', (req, res) => {
     res.send(`Hello Guest`)
+//  res.send(`Hello ${req.params.id}`)
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
