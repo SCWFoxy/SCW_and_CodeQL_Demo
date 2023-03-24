08a882863ddaf0552959a89dad216d04f1b3403f
SCWFoxy <121032351+SCWFoxy@users.noreply.github.com>
2023-03-24 12:17:40 +0000

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const port = 3000
 //Comment Line
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/demo/:id', (req, res) => {
-    res.send(`Hello Guest`)
+ //   To make a change comment out the safe code where it outputs text and undo the comment for the unsafe code that uses a parameter
+ //   Reset by reversing the comment lines
+    
+ //   res.send(`Hello Guest`)
+      res.send(`Hello ${req.params.id}`)
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
